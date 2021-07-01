6cb8b726c2001e5f9d1721b10ace71b5859f8bb0
Nathan Blaz <nathanblaz@gmail.com>
2021-07-01 14:44:06 -0700

    Debugged models, migrations, seeders

---
+++ app/seeds/songs.py
@@ -0,0 +1,42 @@
+from app.models import db, Song
+
+
+def seed_songs():
+    song1 = Song(
+        title='Monolith',
+        song_url='https://tunevillage-app.s3.us-west-1.amazonaws.com/Fugal+-+Parallels+-+01+A1.+Monolith.mp3',
+        song_art='https://tunevillage-app.s3.us-west-1.amazonaws.com/cover.png',
+        user_id=2,
+        pinned=False
+    )
+    song2 = Song(
+        title='Plurality',
+        song_url='https://tunevillage-app.s3.us-west-1.amazonaws.com/Fugal+-+Parallels+-+02+A2.+Plurality.mp3',
+        song_art='https://tunevillage-app.s3.us-west-1.amazonaws.com/cover.png',
+        user_id=2,
+        pinned=False
+    )
+    song3 = Song(
+        title='Parallels',
+        song_url='https://tunevillage-app.s3.us-west-1.amazonaws.com/Fugal+-+Parallels+-+01+A1.+Monolith.mp3',
+        song_art='https://tunevillage-app.s3.us-west-1.amazonaws.com/cover.png',
+        user_id=2,
+        pinned=True
+    )
+    song4 = Song(
+        title='Arrival',
+        song_url='https://tunevillage-app.s3.us-west-1.amazonaws.com/Fugal+-+Parallels+-+04+B2.+Arrival.mp3',
+        song_art='https://tunevillage-app.s3.us-west-1.amazonaws.com/cover.png',
+        user_id=2,
+        pinned=False
+    )
+    db.session.add(song1)
+    db.session.add(song2)
+    db.session.add(song3)
+    db.session.add(song4)
+    db.session.commit()
+
+
+def undo_songs():
+    db.session.execute('TRUNCATE songs RESTART IDENTITY CASCADE;')
+    db.session.commit()
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,14 @@ from app.models import db, User
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
-
+    fugal = User(
+        username='Fugal',
+        email='fugal@secondnatureseattle.com',
+        bio='Berlin-based Korean-American techno artist from Seattle. His style moves in a space of dynamic tension between dense, insistent rhythmics and nuanced emotion. A resident DJ and producer of the singular collective, secondnature.',
+        password='password',
+        avatar='https://imgproxy.ra.co/_/quality:100/h:180/w:180/rt:fill/gravity:sm/plain//images/profiles/fugal.jpg')
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
-
+    db.session.add(fugal)
     db.session.commit()
 
 
