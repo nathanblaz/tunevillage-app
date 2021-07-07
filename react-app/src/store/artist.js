0b05df4b0610a5c3d78c66bd9156eee976445f6e
Nathan Blaz <nathanblaz@gmail.com>
2021-07-07 12:58:06 -0700

    Rough draft of new user store

---
+++ react-app/src/store/artist.js
@@ -0,0 +1,78 @@
+// constants
+const ADD_AVATAR = "artist/ADD_AVATAR";
+const GET_ARTIST = "artist/GET_USER";
+const DELETE_AVATAR = "artist/DELETE)AVATAR";
+
+// action creators
+
+const addAvatar = (avatar) => ({
+  type: ADD_AVATAR,
+  payload: avatar,
+});
+
+const getUser = (artist) => ({
+  type: GET_USER,
+  payload: artist,
+});
+
+const deleteAvatar = (avatar) => ({
+  type: DELETE_AVATAR,
+  payload: avatar,
+});
+
+// thunks
+
+export const getAUser = (artistId) => async (dispatch) => {
+  const res = await fetch(`/api/artists/${artistId}`);
+  if (res.ok) {
+    const artistProfile = await res.json();
+    dispatch(getUser(artistProfile));
+  } else {
+    console.log("error--getAUser thunk");
+    console.log(res);
+  }
+};
+
+export const uploadAvatar = (formData, artistId) => async (dispatch) => {
+  const res = await fetch(`/api/artists/${artistId}/avatar`, {
+    method: "POST",
+    body: formData,
+  });
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(addAvatar(data));
+  } else {
+    console.log("error--uploadAvatar thunk");
+    console.log(res);
+  }
+};
+
+export const removeAvatar = (artistId) => async (dispatch) => {
+  const res = await fetch(`/api/artists/${artistId}/avatar`, {
+    method: "PUT",
+  });
+  if (res.ok) {
+    dispatch(deleteAvatar(artistId));
+  } else {
+    console.log("error--removeAvatar thunk");
+    console.log(res);
+  }
+};
+
+// reducer
+const initialState = {};
+
+export default function artistReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_AVATAR:
+      const newAvatarState = { ...action.payload };
+      return newAvatarState;
+    case DELETE_AVATAR:
+      const removeAvatarState = { ...action.payload };
+      return removeAvatarState;
+    case GET_USER:
+      return { ...action.payload };
+    default:
+      return state;
+  }
+}
