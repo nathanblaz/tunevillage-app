573c4ea1209e0f5b3fadff60ca9fe31151f7c406
Nathan Blaz <nathanblaz@gmail.com>
2021-07-10 08:08:50 -0700

    Wrote create_song() POST route

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -11,3 +11,37 @@ song_routes = Blueprint("songs", __name__)
 def get_one_song(id):
     song = Song.query.get(id)
     return song.to_dict()
+
+
+@song_routes.route('/new', methods=["POST"])
+@login_required
+def create_song():
+    print("****We got to song_artist() on the /api/songs/new route")
+    if "song" not in request.files:
+        return {"errors": "audio file required"}, 400
+
+    song = request.files["song"]
+
+    if not allowed_file(song.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    song.filename = get_unique_filename(song.filename)
+
+    upload = upload_file_to_s3(song)
+
+    if "url" not in upload:
+        return upload, 400
+
+    url = upload["url"]
+
+    new_song = Song(
+        title=request.form["title"],
+        song_art=None,
+        song_url=url,
+        user_id=current_user.id,
+        pinned=false,
+    )
+    db.session.add(new_song)
+    db.session.commit()
+    print("*******new_song is ", new_song)
+    return new_song.to_dict()
