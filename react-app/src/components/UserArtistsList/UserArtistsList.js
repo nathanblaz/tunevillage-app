c8da6c63ae042d4de60e8a869ddb0276b3b46a59
Nathan Blaz <nathanblaz@gmail.com>
2021-07-11 08:39:39 -0700

    UserArtistList rendering

---
+++ react-app/src/components/UserArtistsList/UserArtistsList.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getArtists } from "../../store/artist";
+
+function UserArtistsList() {
+  const dispatch = useDispatch();
+  const artists = Object.values(useSelector((state) => state.artist));
+  const currentUser = useSelector((state) => state.session.user);
+
+  useEffect(() => {
+    dispatch(getArtists());
+  }, [dispatch]);
+
+  console.log("In ArtistsList.js, artists = ", artists);
+
+  const userArtistListItems = artists.map((artist, index) => {
+    return currentUser.id === Number(artist.user_id) ? (
+      <li key={index}>
+        <NavLink to={`/artists/${artist.id}`}>
+          <img src={artist.avatar} alt="avatar"></img>
+          {artist.artistname}
+        </NavLink>
+      </li>
+    ) : null;
+  });
+
+  return (
+    <>
+      <h1>Artist List: </h1>
+      <ul>{userArtistListItems}</ul>
+    </>
+  );
+}
+
+export default UserArtistsList;
